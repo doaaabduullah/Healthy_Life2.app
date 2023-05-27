e00b57969f8ede813f97f1936ca05ee90949b809
doaaabduullah <134365411+doaaabduullah@users.noreply.github.com>
2023-05-27 14:50:12 +0400

    finished project

--- app/src/main/java/com/example/healthylife2/details_Activity.java
+++ app/src/main/java/com/example/healthylife2/details_Activity.java
@@ -7,50 +7,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class details_Activity extends AppCompatActivity {
     Button submitBtn;
-    EditText currentWeight, height, goalWeight;
+    EditText currentWeight ,height,goalWeight;
     DBHelper myDB;
-    DatabaseReference databaseReference;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        currentWeight = findViewById(R.id.currentWeighTxt);
-        height = findViewById(R.id.heightTxt);
-        goalWeight = findViewById(R.id.goalWeightTxt);
-        submitBtn = findViewById(R.id.submitButton);
+        currentWeight=findViewById(R.id.currentWeighTxt);
+        height=findViewById(R.id.heightTxt);
+        goalWeight=findViewById(R.id.goalWeightTxt);
+        submitBtn=findViewById(R.id.submitButton);
 
-        myDB = new DBHelper(this);
-        databaseReference = FirebaseDatabase.getInstance().getReference("personalDetails");
+        myDB=new DBHelper(this);
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View view) {
-                String current = currentWeight.getText().toString();
-                String heights = height.getText().toString();
-                String goal = goalWeight.getText().toString();
-
-                // Save data to Firebase
-                PersonalDetail personalDetail = new PersonalDetail(current, heights, goal);
-                String detailId = databaseReference.push().getKey();
-                databaseReference.child(detailId).setValue(personalDetail);
-
+                String current=currentWeight.getText().toString();
+                String heights=height.getText().toString();
+                String goal=goalWeight.getText().toString();
                 Boolean regResult = myDB.insertPersonalDetail(current, heights, goal);
-                if (regResult) {
-                    Toast.makeText(details_Activity.this, "Your Detail Saved Successfully", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(details_Activity.this, home11.class);
+                if (regResult == true) {
+                    Toast.makeText(details_Activity.this, "Your Detail Saved Successful", Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(details_Activity.this,home11.class);
                     startActivity(intent);
                 } else {
                     Toast.makeText(details_Activity.this, "Your Detail Failed", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
     }
 }
