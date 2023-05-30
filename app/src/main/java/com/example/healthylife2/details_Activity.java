f799c59f4c9a3700dd39e1f076c2cff3f8015d1a
DELL <201811057@gcet.edu.om>
2023-05-30 11:55:55 +0400

    finished project

--- app/src/main/java/com/example/healthylife2/details_Activity.java
+++ app/src/main/java/com/example/healthylife2/details_Activity.java
@@ -15,38 +15,35 @@ import com.google.firebase.database.FirebaseDatabase;
 public class details_Activity extends AppCompatActivity {
     Button submitBtn;
     EditText currentWeight, height, goalWeight;
-    DatabaseReference personalDetailsRef;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
-        currentWeight = findViewById(R.id.currentWeighTxt);
-        height = findViewById(R.id.heightTxt);
-        goalWeight = findViewById(R.id.goalWeightTxt);
+        super. onCreate(savedInstanceState);
+        setContentView(R. layout. activity_details);
+
+        currentWeight = findViewById(R. id. currentWeighTxt);
+        height = findViewById(R.id. heightTxt);
+        goalWeight = findViewById(R.id. goalWeightTxt);
         submitBtn = findViewById(R.id.submitButton);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        personalDetailsRef = database.getReference("personal_details");
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("personalDetails");
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String current = currentWeight.getText().toString();
+                String current = currentWeight. getText(). toString();
                 String heights = height.getText().toString();
-                String goal = goalWeight.getText().toString();
+                String goal = goalWeight. getText().toString();
 
-                // Create a new child node in the "personal_details" table
-                DatabaseReference detailRef = personalDetailsRef.push();
-
-                // Create a data object with the personal details
-                PersonalDetails personalDetails = new PersonalDetails(current, heights, goal);
-
-                // Set the data for the personal details
-                detailRef.setValue(personalDetails);
+                DatabaseReference newDetailRef = databaseReference. push();
+                newDetailRef. child("currentWeight"). setValue(current);
+                newDetailRef. child("height"). setValue(heights);
+                newDetailRef. child("goalWeight"). setValue(goal);
 
                 Toast.makeText(details_Activity.this, "Your Detail Saved Successfully", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(details_Activity.this, home11.class);
+
+                Intent intent = new Intent(details_Activity. this, home11. class);
                 startActivity(intent);
             }
         });
